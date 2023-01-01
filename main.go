f270dc82ec03a8dedf55d11b5d0990a74bb5a287
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-01-01 09:04:33 +0800

    2nd commit

--- main.go
+++ main.go
@@ -36,11 +36,12 @@ func chatgpt() string {
 	}
 	resp, err := c.CreateCompletion(ctx, req)
 	if err != nil {
-		return
-	}
-	fmt.Println(resp.Choices[0].Text)
+		return "You got an error!"
+	} else {
+		fmt.Println(resp.Choices[0].Text)
 
-	return resp.Choices[0].Text
+		return resp.Choices[0].Text
+	}
 
 }
 
